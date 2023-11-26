8ea2cd177c50357d3db97d55029bc725b50c9723
lisayu198 <lisayu198@gmail.com>
2023-11-26 11:01:32 -0800

    added JUnit

---
+++ src/main/java/src/PasswordException.java
@@ -0,0 +1,7 @@
+package src;
+
+public class PasswordException extends Exception {
+    public PasswordException(String message) {
+        super(message);
+    }
+}
--- src/main/java/src/UnavailableBookException.java
+++ src/main/java/src/UnavailableBookException.java
@@ -1,4 +1,4 @@
-
+package src;
 
 public class UnavailableBookException extends Exception {
     public UnavailableBookException(String message) {
